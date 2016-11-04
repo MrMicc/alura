746399c2ad941a28844a88111d883983b84f20c6
Miccieli <luiz.miccieli@gmail.com>
2016-11-04 15:17:04 -0200

    MOD: Adicionado SQLite

--- Node/casadocodigo/app/routes/Produtos.js
+++ Node/casadocodigo/app/routes/Produtos.js
@@ -6,6 +6,24 @@
 module.exports = function (app) {
     app.get('/produtos',function (req,res) {
         console.log('Listando os produtos');
-        res.render('produtos/lista');
+        /*var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host : 'localhost',
+            user: 'root',
+            password: '',
+            database: 'casadocodigo'
+        });*/
+        var sqlite = require('sqlite3');
+        var connection = new sqlite.Database('casadocodigo.db'); //mysql -> createConnection({host: '', user:'', password:'', database:''})
+
+        connection.all('SELECT * FROM livros', function (err, result) { //mysql -> query
+            console.log(err);
+            res.send(result); // encaminhando para a view
+        });
+
+        connection.close(); //mysql -> end
+
+
+        //res.render('produtos/lista');
     })
 };
