0811a93cba097c7628cd2eb6587c2e98b59874ce
Miccieli <luiz.miccieli@gmail.com>
2016-11-08 12:55:42 -0200

    Explicacao do funcionamento do require

--- Node/casadocodigo/config/configExpress.js
+++ Node/casadocodigo/config/configExpress.js
@@ -1,6 +1,12 @@
 /**
  * Created by ter00409 on 03/11/2016.
  */
+/*
+FUNCIONAMENTO DO REQUIRE:
+    O require sem barra ou ponto, ele vai para pasta de modulos do node e ai sim, carrega as bibliotecas
+    caso tenham algum ponto ou barra, será percorrido o path do projeto apartir do path onde se encontra o modulo
+ */
+
 var express = require('express');
 var load = require('express-load');
 var bodyParser = require('body-parser');
