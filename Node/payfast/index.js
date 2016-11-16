f4b1a56b47db27468230fbd8645dacefd3c53f9e
Miccieli <luiz.miccieli@gmail.com>
2016-11-16 08:04:36 -0200

    Modulo REST de pagamento

--- Node/payfast/index.js
+++ Node/payfast/index.js
@@ -4,7 +4,7 @@
 var app = require('./config/custom-express')();//carregando e invocando o codigo ()
 
 var server = app.listen(3000, function () { //setando a porta 3000 como a default da aplicacao
-    console.log('Servidor rodandno na porta: %s', server.address().port)
+    console.log('Servidor rodandno na porta: %s', server.address().port);
 });
 
 
