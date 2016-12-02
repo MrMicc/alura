08c59389eee387f46140a504cbeee1be320647d3
Miccieli <luiz.miccieli@gmail.com>
2016-12-02 08:21:20 -0200

    Angular - adicional o painel das fotos

--- alurapic/public/js/controllers/fotos-controller.js
+++ alurapic/public/js/controllers/fotos-controller.js
@@ -5,6 +5,6 @@ angular.module('alurapic').controller('FotosController', function ($scope) {
 
     $scope.fotos = {
         url: 'http://www.fundosanimais.com/Minis/leoes.jpg',
-        alt: 'Leão'
+        titulo: 'Leão'
     }
 });
