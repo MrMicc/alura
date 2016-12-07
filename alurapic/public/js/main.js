dd5ee49dfb4237900e2fef752848ee35aaa0ac8e
Miccieli <luiz.miccieli@gmail.com>
2016-12-06 22:10:47 -0200

    Angular - ADD - criadas ng-views

--- alurapic/public/js/main.js
+++ alurapic/public/js/main.js
@@ -1,4 +1,17 @@
 /**
  * Created by luizmiccieli on 30/11/16.
  */
-angular.module('alurapic',['minhasDiretivas', 'ngAnimate']);
+angular.module('alurapic',['minhasDiretivas', 'ngAnimate', 'ngRoute']).config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+
+    $routeProvider.when('/', {
+        templateUrl : './partials/principal.html',
+        controller : 'FotosController'
+    });
+
+    $routeProvider.when('/fotos/new', {
+        templateUrl : './partials/newFoto.html'
+    });
+
+
+});
